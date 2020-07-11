9b2363cc36e0c1831fccb16abec282ba9e606481
aayu5hh <me.aayus@gmail.com>
2020-07-11 15:50:12 -0500

    Order pojo and order status

--- src/main/java/miu/edu/cs545waa/domain/Order.java
+++ src/main/java/miu/edu/cs545waa/domain/Order.java
@@ -1,4 +1,93 @@
 package miu.edu.cs545waa.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Entity
 public class Order {
+
+    @Id
+    private long id;
+    private Date orderDate;
+    private OrderStatus status;
+    private Buyer buyer;
+    private Seller seller;
+    private Address BillingAddress;
+    private Address ShippingAddress;
+    private Payment payment;
+    private List<OrderItem> items = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public Address getBillingAddress() {
+        return BillingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        BillingAddress = billingAddress;
+    }
+
+    public Address getShippingAddress() {
+        return ShippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        ShippingAddress = shippingAddress;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
 }
---
+++ src/main/java/miu/edu/cs545waa/domain/OrderStatus.java
@@ -0,0 +1,8 @@
+package miu.edu.cs545waa.domain;
+
+public enum  OrderStatus {
+    New,
+    Delivered,
+    Shipped,
+    Cancelled
+}
