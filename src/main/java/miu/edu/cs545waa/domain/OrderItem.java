ea6f123982e91409da1efa7463ae933f91816b44
aayu5hh <me.aayus@gmail.com>
2020-07-12 08:55:32 -0500

    Order classes

--- src/main/java/miu/edu/cs545waa/domain/OrderItem.java
+++ src/main/java/miu/edu/cs545waa/domain/OrderItem.java
@@ -1,23 +1,49 @@
 package miu.edu.cs545waa.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.OneToOne;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class OrderItem {
-    @GeneratedValue
-    public Long id;
+@Entity
+public class CartItem {
 
-    private int quantity;
-
-    private double price;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @OneToOne
     private Product product;
 
+    private int quantity;
+    private double totalPrice;
+
+    public CartItem() {}
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = quantity * product.getPrice();
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return totalPrice;
+    }
+
+    public void setPrice(double price) {
+        this.totalPrice = price;
+    }
 }
